27c6ad02fd7a1c0b3413d3cfd38a5f9d9735e791
cathymiao04 <cathy@puygroup.com>
2024-07-29 17:31:30 -0500

    Finished implementation of functionality

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React from 'react';
+import './About.css';
 
 function About() {
   return (
-    <div>
+    <section className='about'>
+      <div className='container'>
+        <div className='section-title'>
+          <h2>About</h2>
+        </div>
 
-    </div>
+        <div className='about-text'>
+          <h2 className='about-title fs-26 ls-1'>
+            About BookFinder
+          </h2>
+          <p className='fs-17'>
+            BookFinder uses the OpenLibrary API to find books.
+          </p>
+        </div>
+      </div>
+    </section>
   )
 }
 
